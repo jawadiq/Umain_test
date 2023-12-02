334b3744497c2be206724fc945448a1e03e90b25
Jawad Iqbal <33781464+jawadiq@users.noreply.github.com>
2023-12-02 17:48:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/umain_test/network/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.umain_test.network
+
+ class Constants {
+     companion object{
+         var BASE_URL = "https://food-delivery.umain.io/api/v1/"
+         var NEW_USER = "/api/update_name_and_email"
+         var PASSCODE = "/api/get_passcode"
+     }
+
+
+}
