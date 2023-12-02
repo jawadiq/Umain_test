9919989855fbf495bb2d99eb9b2c3c04bc375538
Jawad Iqbal <33781464+jawadiq@users.noreply.github.com>
2023-12-02 17:49:59 +0100

    base url changes

--- app/src/main/java/com/example/umain_test/network/Constants.kt
+++ app/src/main/java/com/example/umain_test/network/Constants.kt
@@ -3,8 +3,7 @@ package com.example.umain_test.network
  class Constants {
      companion object{
          var BASE_URL = "https://food-delivery.umain.io/api/v1/"
-         var NEW_USER = "/api/update_name_and_email"
-         var PASSCODE = "/api/get_passcode"
+
      }
 
 
